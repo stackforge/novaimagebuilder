0436ddbe806cbbc7d461395603461c9b402b6e23
Ian McLeod <imcleod@redhat.com>
2013-04-02 23:21:48 -0500

    Add support for creating volume snapshots Add initial support for using ISO install media Add example of Fedora 18 ks file that uses ISO install media Add RHEL5 and RHEL6 example files Add significant documentation to README Add license Split into CLI file and utility functions

--- glance_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./create_glance_image.py --username <FIXME> --tenant <FIXME> --password <FIXME> --auth-url <FIXME> \
-                         --glance-url <FIXME> --admin-password <FIXME> $1
-
-
