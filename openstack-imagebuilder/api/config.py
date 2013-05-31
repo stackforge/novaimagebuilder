2ee48214b776578141dcae3b515ddd1cd393cb7e
Steve Loranz <sloranz@xanham.com>
2013-05-31 11:42:38 -0500

    additions from pecan test project

---
+++ openstack-imagebuilder/api/config.py
@@ -0,0 +1,45 @@
+# Server Specific Configurations
+server = {
+    'port': '8080',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'openstack-imagebuilder.api.controllers.RootController',
+    'modules': ['openstack-imagebuilder.api'],
+    'static_root': '%(confdir)s/public',
+    'template_path': '%(confdir)s/api/templates',
+    'debug': True,
+    'errors': {
+        404: '/error/404',
+        '__force_dict__': True
+    }
+}
+
+logging = {
+    'loggers': {
+        'root': {'level': 'INFO', 'handlers': ['console']},
+        'pdiddy': {'level': 'DEBUG', 'handlers': ['console']}
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        }
+    }
+}
+
+# Custom Configurations must be in Python dictionary format::
+#
+# foo = {'bar':'baz'}
+#
+# All configurations are accessible at::
+# pecan.conf
